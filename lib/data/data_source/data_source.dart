50ba1e7df721ed411986bbba3f8ce5f613553ae6
MouhamedBourouba <mouhamedMobileDev@gmail.com>
2023-06-18 18:41:26 +0100

    reworked local data source class

--- lib/data/data_source/data_source.dart
+++ lib/data/data_source/data_source.dart
@@ -1,159 +1,131 @@
-import 'package:ClassConnect/data/data_source/cloud_data_source.dart';
-import 'package:ClassConnect/data/data_source/local_data_source.dart';
 import 'package:ClassConnect/data/model/class.dart';
 import 'package:ClassConnect/data/model/class_message.dart';
-import 'package:ClassConnect/data/model/error.dart';
 import 'package:ClassConnect/data/model/user.dart';
 import 'package:ClassConnect/data/model/user_event.dart';
-import 'package:ClassConnect/di/di.dart';
-import 'package:ClassConnect/utils/extension.dart';
-import 'package:ClassConnect/utils/utils.dart';
-import 'package:multiple_result/multiple_result.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-abstract class DataBaseRepository {
-  Future<Result<Unit, String>> storeData<T>(Object data);
+final user = User.defaultUser();
 
-  Future<Result<List<T>, String>> getObjects<T>();
+final message = ClassMessage(
+  id: "dshg",
+  streamMessagesId: "gsd",
+  senderId: "dhq",
+  senderName: "gg seznder",
+  sentTimeMS: "df",
+  title: "fdsh",
+);
+final event = UserEvent(
+  seen: false,
+  eventType: EventType.classMemberShipInvitation,
+  eventReceiverId: "dfh",
+  eventSenderId: "fdhs",
+  id: "hfds",
+);
+final ggClass = Class(
+  id: "gg",
+  teachers: [],
+  streamMessagesId: "dsh",
+  studentsIds: ["fdqd", "sdgs"],
+  homeWorkId: "sdgq",
+  bannedStudents: ["dsg"],
+  className: "gg class",
+  subject: "fdhfsd",
+);
 
-  Future<Result<T, String>> getObject<T>(String id);
+void TestingFun() async {
+  final localDB = LocalDataSource();
+  await localDB.init();
+  print((await localDB.getAllObjects<Class>()).length);
+  print((await localDB.getObjectById<Class>("gg")).className);
 }
 
-class DataBaseImpl implements DataBaseRepository {
-  final CloudDataSource cloudDataSource = getIt();
-  final LocalDataSource localDataSource = getIt();
+enum MDataTable {
+  users,
+  classes,
+  events,
+  messages,
+}
 
-  @override
-  Future<Result<Unit, String>> storeData<T>(Object data) async {
-    if (await isOnline()) {
-      await cloudDataSource.appendRow(dataToMap(data), typeToTable<T>());
-    } else {
-      return Result.error(MException.noInternetConnection().errorMessage);
-    }
-    if (data is User) {
-      localDataSource.addUser(data);
-    } else if (data is Class) {
-      localDataSource.addClass(data);
-    } else if (data is UserEvent) {
-      localDataSource.addEvent(data);
-    } else if (data is ClassMessage) {
-      localDataSource.addClassMessage(data);
-    }
-    return Result.success(unit);
+MDataTable dataTableFromType(Type type) {
+  if (type == User) {
+    return MDataTable.users;
+  } else if (type == Class) {
+    return MDataTable.classes;
+  } else if (type == UserEvent) {
+    return MDataTable.events;
+  } else if (type == ClassMessage) {
+    return MDataTable.messages;
+  } else {
+    throw UnsupportedError("Unsupported type");
+  }
+}
+
+String idFromObject(Object data) {
+  switch (data.runtimeType) {
+    case User:
+      return (data as User).id;
+    case Class:
+      return (data as Class).id;
+    case UserEvent:
+      return (data as UserEvent).id;
+    case ClassMessage:
+      return (data as ClassMessage).id;
+    default:
+      throw UnsupportedError('unsupported Type ${data.runtimeType}');
+  }
+}
+
+class LocalDataSource {
+  late Box appBox;
+  late Box<User> usersBox;
+  late Box<Class> classesBox;
+  late Box<ClassMessage> classMessagesBox;
+  late Box<UserEvent> eventBox;
+  final User? currentUser = null;
+
+  Future<void> init() async {
+    await Hive.initFlutter();
+    Hive.registerAdapter(UserAdapter());
+    Hive.registerAdapter(ClassAdapter());
+    Hive.registerAdapter(ClassMessageAdapter());
+    Hive.registerAdapter(EventTypeAdapter());
+    Hive.registerAdapter(UserEventAdapter());
+    appBox = await Hive.openBox("app_box");
+    usersBox = await Hive.openBox("users");
+    classesBox = await Hive.openBox("classes");
+    classMessagesBox = await Hive.openBox("class_messages");
+    eventBox = await Hive.openBox("events");
   }
 
-  @override
-  Future<Result<T, String>> getObject<T>(String id) async {
-    if (await isOnline()) {
-      final dataJson = await cloudDataSource.getRow(typeToTable<T>(), rowKey: id);
-      final data = mapToData<T>(dataJson!);
-      return Result.success(data);
-    } else {
-      if (T is User) {
-        return Result.success(
-          localDataSource.getUsers().where((element) => element.id == id).first as T,
-        );
-      } else if (T == Class) {
-        return Result.success(
-          localDataSource.getClasses().where((element) => element.id == id).first as T,
-        );
-      } else if (T == UserEvent) {
-        return Result.success(
-          localDataSource.getEvents().where((element) => element.id == id).first as T,
-        );
-      } else if (T == ClassMessage) {
-        return Result.success(
-          localDataSource.getClassMessages().where((element) => element.id == id).first as T,
-        );
-      } else {
-        return Result.success(
-          localDataSource.getUsers().where((element) => element.id == id).first as T,
-        );
-      }
-    }
-  }
-
-  @override
-  Future<Result<List<T>, String>> getObjects<T>() async {
-    if (await isOnline()) {
-      final jsonList = await cloudDataSource.getAllRows(typeToTable<T>());
-      final list = jsonList!.map((e) {
-        if (T == User) {
-          return e.toUser();
-        } else if (T == Class) {
-          return e.toClass();
-        } else if (T == UserEvent) {
-          return UserEvent.fromMap(e);
-        } else if (T == ClassMessage) {
-          return ClassMessage.fromMap(e);
-        }
-        return e.toClass();
-      });
-      final castedList = list.map((e) => e as T);
-      return Result.success(castedList.toList());
-    } else {
-      if (T is User) {
-        return Result.success(
-          localDataSource.getUsers().map((e) => e as T).toList(),
-        );
-      } else if (T == Class) {
-        return Result.success(
-          localDataSource.getClasses().map((e) => e as T).toList(),
-        );
-      } else if (T == UserEvent) {
-        return Result.success(
-          localDataSource.getEvents().map((e) => e as T).toList(),
-        );
-      } else if (T == ClassMessage) {
-        return Result.success(
-          localDataSource.getClassMessages().map((e) => e as T).toList(),
-        );
-      } else {
-        return Result.success(
-          localDataSource.getUsers().map((e) => e as T).toList(),
-        );
-      }
+  Box<T> boxFromType<T>(Type type) {
+    switch (T) {
+      case User:
+        return usersBox as Box<T>;
+      case Class:
+        return classesBox as Box<T>;
+      case UserEvent:
+        return eventBox as Box<T>;
+      case ClassMessage:
+        return classMessagesBox as Box<T>;
+      default:
+        throw UnsupportedError("Unsupported type: $T");
     }
   }
 
-  T mapToData<T>(Map<String, String> map) {
-    if (T == User) {
-      return map.toUser() as T;
-    } else if (T == Class) {
-      return map.toClass() as T;
-    } else if (T == UserEvent) {
-      return UserEvent.fromMap(map) as T;
-    } else if (T == ClassMessage) {
-      return ClassMessage.fromMap(map) as T;
-    } else {
-      return "" as T;
-    }
+  Future<Iterable<T>> getAllObjects<T>() async {
+    return boxFromType<T>(T).values;
   }
 
-  Map<String, dynamic> dataToMap(Object data) {
-    if (data is User) {
-      return data.toMap();
-    } else if (data is Class) {
-      return data.toMap();
-    } else if (data is UserEvent) {
-      return data.toMap();
-    } else if (data is ClassMessage) {
-      return data.toMap();
-    } else {
-      return {};
-    }
+  Future<bool> storeObject<T extends Object>(T data) async {
+    await boxFromType<T>(T).put(idFromObject(data), data);
+    return true;
   }
 
-  MTable typeToTable<data>() {
-    if (data is User) {
-      return MTable.usersTable;
-    } else if (data is Class) {
-      return MTable.classesTable;
-    } else if (data is UserEvent) {
-      return MTable.eventsTable;
-    } else if (data is ClassMessage) {
-      return MTable.streamMessagesTable;
-    } else {
-      return MTable.usersTable;
-    }
+  Future<T> getObjectById<T extends Object>(String id) async {
+    final data = (await getAllObjects<T>()).where((element) => idFromObject(element) == id);
+    if (data.isEmpty) throw Exception("cant find object with id: $id");
+    return data.first;
   }
+
+  Future<void> putDataToAppBox(String key, dynamic value) => appBox.put(key, value);
 }
